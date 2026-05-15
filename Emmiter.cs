0b3709f519671f92aa520a957c68d340cccd7651
Kirill_A <kirilla1nx@yandex.ru>
2026-05-15 23:50:47 +0800

    Система частиц, часть 1, сделано

---
+++ Emmiter.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static particles.Particle;
+
+namespace particles
+{
+
+    public class Emmiter
+    {
+        // собственно список, пока пустой
+        List<Particle> particles = new List<Particle>();
+        public int MousePositionX;
+        public int MousePositionY;
+
+        public float GravitationX = 0;
+        public float GravitationY = 0; // пусть гравитация будет силой один пиксель за такт, нам хватит
+
+        public List<IImpactPoint> impactPoints = new List<IImpactPoint>(); // <<< ТАК ВОТ
+        public void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                particle.Life -= 1; // уменьшаю здоровье
+                                    // если здоровье кончилось
+                if (particle.Life < 0)
+                {
+                    ResetParticle(particle); // заменили этот блок на вызов сброса частицы
+                }
+                else
+                {
+                    // каждая точка по-своему воздействует на вектор скорости
+                    foreach (var point in impactPoints)
+                    {
+                        point.ImpactParticle(particle);
+                    }
+
+                    // гравитация воздействует на вектор скорости, поэтому пересчитываем его
+                    particle.SpeedX += GravitationX;
+                    particle.SpeedY += GravitationY;
+
+                    // и добавляем новый, собственно он даже проще становится, 
+                    // так как теперь мы храним вектор скорости в явном виде и его не надо пересчитывать
+                    particle.X += particle.SpeedX;
+                    particle.Y += particle.SpeedY;
+                }
+            }
+
+            // добавил генерацию частиц
+            // генерирую не более 10 штук за тик
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < ParticlesCount) // пока частиц меньше 500 генерируем новые
+                {
+                    // а у тут уже наш новый класс используем
+                    var particle = new ParticleColorful();
+                    // ну и цвета меняем
+                    particle.FromColor = Color.White;
+                    particle.ToColor = Color.FromArgb(0, Color.Black);
+                    //particle.X = MousePositionX;
+                    //particle.Y = MousePositionY;
+                    ResetParticle(particle); // добавили вызов ResetParticle
+
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break; // а если частиц уже 500 штук, то ничего не генерирую
+                }
+            }
+
+        }
+        public void Render(Graphics g)
+        {
+
+            // это то же самое что на форме в методе Render
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+
+            // рисую точки притяжения красными кружочками
+            foreach (var point in impactPoints)
+            {
+                point.Render(g); // это добавили
+            }
+        }
+
+
+        public int ParticlesCount = 800;
+
+        // добавил новый метод, виртуальным, чтобы переопределять можно было
+        public virtual void ResetParticle(Particle particle)
+        {
+            // восстанавливаю здоровье
+            particle.Life = 20 + Particle.rand.Next(100);
+
+            // новое начальное расположение частицы — это то, куда указывает курсор
+            particle.X = MousePositionX;
+            particle.Y = MousePositionY;
+
+            /* ЭТО ДОБАВЛЯЮ, тут сброс состояния частицы */
+            var direction = (double)Particle.rand.Next(360);
+            var speed = 1 + Particle.rand.Next(10);
+
+            particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+            particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+            /* конец ЭТО ДОБАВЛЯЮ  */
+
+            particle.Radius = 2 + Particle.rand.Next(10);
+        }
+
+        public class TopEmitter : Emmiter
+        {
+            public int Width; // длина экрана
+
+            public override void ResetParticle(Particle particle)
+            {
+                base.ResetParticle(particle); // вызываем базовый сброс частицы, там жизнь переопределяется и все такое
+
+                // а теперь тут уже подкручиваем параметры движения
+                particle.X = Particle.rand.Next(Width); // позиция X -- произвольная точка от 0 до Width
+                particle.Y = 0;  // ноль -- это верх экрана 
+
+                particle.SpeedY = 1; // падаем вниз по умолчанию
+                particle.SpeedX = Particle.rand.Next(-2, 2); // разброс влево и вправа у частиц 
+            }
+        }
+    
+    }
+}
---
+++ Particle.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace particles
+{
+    public class Particle
+    {
+
+        public int Radius; // радиус частицы
+        public float X; // X координата положения частицы в пространстве
+        public float Y; // Y координата положения частицы в пространстве
+
+        public float SpeedX; // скорость перемещения по оси X
+        public float SpeedY; // скорость перемещения по оси Y
+
+
+        public float Life; // запас здоровья частицы
+
+        // добавили генератор случайных чисел
+        public static Random rand = new Random();
+
+        // конструктор по умолчанию будет создавать кастомную частицу
+        public Particle()
+        {
+            // генерируем произвольное направление и скорость
+            var direction = (double)rand.Next(360);
+            var speed = 1 + rand.Next(10);
+
+            // рассчитываем вектор скорости
+            SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+            SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+
+            Radius = 2 + rand.Next(10);
+            Life = 20 + rand.Next(100); // Добавили исходный запас здоровья от 20 до 120
+        }
+
+        // тут добавил слово virtual чтобы переопределить функцию
+        public virtual void Draw(Graphics g)
+        {
+            /* ... */
+        }
+
+        // новый класс для цветных частиц
+        public class ParticleColorful : Particle
+        {
+            // два новых поля под цвет начальный и конечный
+            public Color FromColor;
+            public Color ToColor;
+
+            // для смеси цветов
+            public static Color MixColor(Color color1, Color color2, float k)
+            {
+                return Color.FromArgb(
+                    (int)(color2.A * k + color1.A * (1 - k)),
+                    (int)(color2.R * k + color1.R * (1 - k)),
+                    (int)(color2.G * k + color1.G * (1 - k)),
+                    (int)(color2.B * k + color1.B * (1 - k))
+                );
+            }
+            public override void Draw(Graphics g)
+            {
+                // рассчитываем коэффициент прозрачности по шкале от 0 до 1.0
+                float k = Math.Min(1f, Life / 100);
+                // рассчитываем значение альфа канала в шкале от 0 до 255
+                // по аналогии с RGB, он используется для задания прозрачности
+                int alpha = (int)(k * 255);
+
+                // создаем цвет из уже существующего, но привязываем к нему еще и значение альфа канала
+                var color = MixColor(ToColor, FromColor, k);
+                var b = new SolidBrush(color);
+
+                // нарисовали залитый кружок радиусом Radius с центром в X, Y
+                g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+
+                // удалили кисть из памяти, вообще сборщик мусора рано или поздно это сам сделает
+                // но документация рекомендует делать это самому
+                b.Dispose();
+            }
+        }
+    }
+}
